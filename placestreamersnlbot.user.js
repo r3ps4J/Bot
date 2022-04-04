40a127bfb67c3ce761f07300c37507e438c9d8c4
r3ps4J <40723838+r3ps4J@users.noreply.github.com>
2022-04-04 19:50:59 +0200

    Missed domains

--- placestreamersnlbot.user.js
+++ placestreamersnlbot.user.js
@@ -7,7 +7,7 @@
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @connect      reddit.com
-// @connect      r3ps4j.nl:3987
+// @connect      r3ps4j.nl
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
@@ -119,7 +119,7 @@ let getPendingWork = (work, rgbaOrder, rgbaCanvas) => {
 
 function connectSocket() {
     Toastify({
-        text: 'Verbinden met PlaceNL server...',
+        text: 'Verbinden met r3ps4J server...',
         duration: DEFAULT_TOAST_DURATION_MS
     }).showToast();
 
@@ -127,7 +127,7 @@ function connectSocket() {
 
     socket.onopen = function () {
         Toastify({
-            text: 'Verbonden met PlaceNL server!',
+            text: 'Verbonden met r3ps4J server!',
             duration: DEFAULT_TOAST_DURATION_MS
         }).showToast();
         socket.send(JSON.stringify({ type: 'getmap' }));
